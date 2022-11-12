c6fad4f492490b4784560d0586ddcdc744eee62e
Ahram-Kim <ahram0223@gmail.com>
2022-11-12 16:17:24 +0900

    2022.11.12 ~46:23

---
+++ src/components/Feed.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+import Sidebar from "./Sidebar";
+
+const Feed = () => {
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar />
+        // ~46:23
+        
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright © 2022 JSM Media
+        </Typography>
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
